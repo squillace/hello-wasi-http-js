4fb78aa7698a6117bf2a0969c11c0c8e5a3c9343
ralph squillace <ralph@squillace.com>
2024-01-19 21:35:27 +0100

    getting things in position for the weekend: DOES NOT WORK

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Hello wasi:http from JavaScript
+# Hello WASI:HTTP from JavaScript
+This repository contains a simple wasi:http implementation of a web server in JavaScript using the @bytecodealliance/preview2-shim types and @bytecodealliance/jco tooling to build the wasm component. This component should work in any wasm component runtime, but for now that's wasmtime or jco (node). 
+
+## Requirements
+
+## Building
+
+## Running
---
+++ index.js
@@ -0,0 +1,19 @@
+import { incomingHandler, types as types$1 } from '@bytecodealliance/preview2-shim/http';
+
+const { handle } = incomingHandler;
+const { Fields,
+  FutureIncomingResponse,
+  IncomingBody,
+  Incoming,
+  OutgoingRequest,
+  RequestOptions } = types$1;
+const { Error: Error$1 } = error;
+const { Pollable } = poll;
+const { InputStream,
+  OutputStream } = streams;
+
+export const incomingHandler = {
+  handle(request, response) {
+    
+  },
+};
